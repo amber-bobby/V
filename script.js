de9a0ef5018a8c29688f4b4356b9d57e26807107
Amber Gill <119124736+amber-bobby@users.noreply.github.com>
2025-02-03 22:11:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.valentines').addEventListener('click', function() {
+    const card = document.querySelector('.card');
+    card.classList.toggle('active'); // Toggle the hover effect on click
+});
